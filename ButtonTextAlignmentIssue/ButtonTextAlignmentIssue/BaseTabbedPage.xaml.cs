5d0b9688d5501c7a07edcd4b7b4af9914b5d1562
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2019-10-30 09:44:10 -0400

    added ButtonIssue

---
+++ ButtonTextAlignmentIssue/ButtonTextAlignmentIssue/BaseTabbedPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ButtonTextAlignmentIssue
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BaseTabbedPage : TabbedPage
+    {
+        public BaseTabbedPage()
+        {
+
+            InitializeComponent();
+            var page1 = new NavigationPage(new ContentPage() { Title = "Tab 1", Content = new Label { Text = "page 1" } }) { Title = "Tab1" };
+            var page2 = new NavigationPage(new MainPage() { Title = "Tab 2" }) { Title = "Tab 2" };
+            var page3 = new NavigationPage(new ContentPage() { Title = "Tab 3", Content = new Label { Text = "page 3" } }) { Title = "Tab 3" };
+            var page4 = new NavigationPage(new MainPage() { Title = "Tab 4" }) { Title = "Tab 4" };
+            var page5 = new NavigationPage(new ContentPage() { Title = "Tab 5", Content = new Label { Text = "page 5" } }) { Title = " Tab 5" };
+            this.Children.Add(page1);
+            this.Children.Add(page2); 
+           
+            this.Children.Add(page3);
+            this.Children.Add(page4);
+
+            this.Children.Add(page5);
+
+        }
+    }
+}
---
+++ ButtonTextAlignmentIssue/ButtonTextAlignmentIssue/MainViewModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using MvvmCross.ViewModels;
+using Xamarin.Forms;
+
+namespace ButtonTextAlignmentIssue
+{
+    public class MainViewModel : MvxViewModel
+    {
+
+        private Color SelectedBackgroundColor => Color.Red;
+        private Color UnselectedBackgroundColor => Color.White;
+        private Color SelectedTextColor => Color.White;
+        private Color UnselectedTextColor => Color.Red;
+
+        public MainViewModel()
+        {
+
+
+
+            OnPrimarialClicked();
+            PrimarialCommand = new Command(OnPrimarialClicked);
+            Secondary_ButtonCommand = new Command(OnSecondary_ButtonClicked);
+        }
+
+        public ContentView ContentView { get; set; }
+
+        public Command PrimarialCommand { get; set; }
+
+        public Command Secondary_ButtonCommand { get; set; }
+
+        public Color PrimarialBackgroundColor { get; set; }
+
+        public Color PrimarialTextColor { get; set; }
+
+        public Color SecondaryBackgroundColor { get; set; }
+
+        public Color SecondaryTextColor { get; set; }
+
+        public Color SecondaryBorderColor { get; set; }
+
+        public Color PrimarialBorderColor { get; set; }
+
+        //public string Count => Colors.Count <= 0 ? string.Empty : Colors.Count.ToString();
+        //public string Image => "ShoppingPrimarial.png";
+
+        //public ObservableCollection<Models.ViewColor> Colors { get; set; } = new ObservableCollection<Models.ViewColor>();
+
+        private void OnPrimarialClicked()
+        {
+            ContentView = new ContentView() { Content = new Label() { Text = "Primarial" , TextColor= Color.Red} };
+            RaisePropertyChanged(nameof(ContentView));
+
+            SetPrimarialStyle(SelectedBackgroundColor, SelectedTextColor, SelectedBackgroundColor);
+            SetSecondary_ButtonStyle(UnselectedBackgroundColor, UnselectedTextColor, UnselectedTextColor);
+        }
+
+        private void OnSecondary_ButtonClicked()
+        {
+            ContentView = new ContentView() { Content = new Label() { Text = "Secondary button", TextColor = Color.Red } };
+            RaisePropertyChanged(nameof(ContentView));
+
+            SetPrimarialStyle(UnselectedBackgroundColor, UnselectedTextColor, UnselectedTextColor);
+            SetSecondary_ButtonStyle(SelectedBackgroundColor, SelectedTextColor, SelectedBackgroundColor);
+        }
+
+        private void SetPrimarialStyle(Color backgroundColor, Color textColor, Color bordercolor)
+        {
+            PrimarialBackgroundColor = backgroundColor;
+            PrimarialTextColor = textColor;
+            PrimarialBorderColor = bordercolor;
+
+            RaisePropertyChanged(nameof(PrimarialBackgroundColor));
+            RaisePropertyChanged(nameof(PrimarialTextColor));
+            RaisePropertyChanged(nameof(PrimarialBorderColor));
+        }
+
+        private void SetSecondary_ButtonStyle(Color backgroundColor, Color textColor, Color bordercolor)
+        {
+            SecondaryBackgroundColor = backgroundColor;
+            SecondaryTextColor = textColor;
+            SecondaryBorderColor = bordercolor;
+
+            RaisePropertyChanged(nameof(SecondaryBackgroundColor));
+            RaisePropertyChanged(nameof(SecondaryTextColor));
+            RaisePropertyChanged(nameof(SecondaryBorderColor));
+        }
+
+        ////public Command OrderCommand { get; set; }
+
+        //public bool HasFavorites => Colors.Any();
+
+        //private void FavoritesChanged(object sender, PropertyChangedEventArgs e)
+        //{
+        //    SetColorsList();
+        //    if (ContentView is Primarial)
+        //    {
+        //        OnPrimarialClicked();
+        //    }
+        //    else
+        //    {
+        //        OnSecondary ButtonClicked();
+        //    }
+        //}
+
+
+
+
+    }
+
+}
