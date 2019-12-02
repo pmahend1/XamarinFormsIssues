15c0ba90dd3743208c4a2c732674ea34dfa5c6ef
Prateek <prateek.mahendrakar@gmail.com>
2019-12-02 13:48:39 -0500

    SkiaImageSourcehandlerIssue added

---
+++ SkiaImageSourceHandlerIssue/SkiaImageSourceHandlerIssue.iOS/UIViewController1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+
+using CoreFoundation;
+using UIKit;
+using Foundation;
+
+namespace SkiaImageSourceHandlerIssue.iOS
+{
+    [Register("UniversalView")]
+    public class UniversalView : UIView
+    {
+        public UniversalView()
+        {
+            Initialize();
+        }
+
+        public UniversalView(RectangleF bounds) : base(bounds)
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            BackgroundColor = UIColor.Red;
+        }
+    }
+
+    [Register("UIViewController1")]
+    public class UIViewController1 : UIViewController
+    {
+        public UIViewController1()
+        {
+            var test = new SKImageSourceHandler();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            View = new UniversalView();
+
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view
+        }
+    }
+}
